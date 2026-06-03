d7f7342717ca3fc60f02e78c9025c598bdb20746
pedram <pedramnaghib@gmail.com>
2026-06-03 13:20:14 +0300

    minor bug fix

--- main.py
+++ main.py
@@ -11,7 +11,7 @@ from src.bot.handlers import register_bot_handlers
 from src.database.db_manager import init_db
 
 # 🎛 تنظیمات ران شدن (روی سیستم خودت False بگذار، روی سرور رندر True)
-USE_WEBHOOK = False
+USE_WEBHOOK = True
 
 WEBHOOK_HOST = "anonai-bot.onrender.com"
 WEBHOOK_PORT = int(os.environ.get("PORT", 8000))
